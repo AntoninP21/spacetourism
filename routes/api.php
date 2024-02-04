5255d9e1d1ca832726da2b2cc0f3b6870a088140
AntoninP21 <antoninporcher21@gmail.com>
2024-02-04 19:57:04 +0100

    Ajout du systeme de login pour l'API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\PlanetAPIController;
 use App\Http\Controllers\API\CrewAPIController;
 use App\Http\Controllers\API\TechnologieAPIController;
+use App\Http\Controllers\API\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,36 +17,34 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::group(['prefix' => '/planet'],function () {
-    Route::get('/', [PlanetAPIController::class, 'index'])->name('index');
-    Route::get('/{id}', [PlanetAPIController::class, 'show']);
-    Route::post('/', [PlanetAPIController::class, 'store']);
-    Route::put('/{id}', [PlanetAPIController::class, 'update']);
-    Route::delete('/{id}', [PlanetAPIController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::group(['prefix' => '/planet'],function () {
+        Route::get('/', [PlanetAPIController::class, 'index'])->name('index');
+        Route::get('/{id}', [PlanetAPIController::class, 'show']);
+        Route::post('/', [PlanetAPIController::class, 'store']);
+        Route::put('/{id}', [PlanetAPIController::class, 'update']);
+        Route::delete('/{id}', [PlanetAPIController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => '/crew'],function () {
+        Route::get('/', [CrewAPIController::class, 'index'])->name('index');
+        Route::get('/{id}', [CrewAPIController::class, 'show']);
+        Route::post('/', [CrewAPIController::class, 'store']);
+        Route::put('/{id}', [CrewAPIController::class, 'update']);
+        Route::delete('/{id}', [CrewAPIController::class, 'destroy']);
+    });
+    Route::group(['prefix' => '/technologie'],function () {
+        Route::get('/', [TechnologieAPIController::class, 'index'])->name('index');
+        Route::get('/{id}', [TechnologieAPIController::class, 'show']);
+        Route::post('/', [TechnologieAPIController::class, 'store']);
+        Route::put('/{id}', [TechnologieAPIController::class, 'update']);
+        Route::delete('/{id}', [TechnologieAPIController::class, 'destroy']);
+    });
+
+
+Route::post('register',[UserAuthController::class,'register']);
+Route::post('login',[UserAuthController::class,'login']);
+Route::post('logout',[UserAuthController::class,'logout'])
+  ->middleware('auth:sanctum');
 });
 
-Route::group(['prefix' => '/crew'],function () {
-    Route::get('/', [CrewAPIController::class, 'index'])->name('index');
-    Route::get('/{id}', [CrewAPIController::class, 'show']);
-    Route::post('/', [CrewAPIController::class, 'store']);
-    Route::put('/{id}', [CrewAPIController::class, 'update']);
-    Route::delete('/{id}', [CrewAPIController::class, 'destroy']);
-});
-Route::group(['prefix' => '/technologie'],function () {
-    Route::get('/', [TechnologieAPIController::class, 'index'])->name('index');
-    Route::get('/{id}', [TechnologieAPIController::class, 'show']);
-    Route::post('/', [TechnologieAPIController::class, 'store']);
-    Route::put('/{id}', [TechnologieAPIController::class, 'update']);
-    Route::delete('/{id}', [TechnologieAPIController::class, 'destroy']);
-});
-
-Route::group(['prefix' => '/crew'],function () {
-    Route::get('/index', [PlanetAPIController::class, 'index']);
-});   
-Route::group(['prefix' => '/technologie'],function () {
-    Route::get('/index', [PlanetAPIController::class, 'index']);
-}); 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
